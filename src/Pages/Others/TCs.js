515d2c5b3fccaa41c56afad1a4e78d5bfc4ece73
Hussseinkizz <hssnkizz@gmail.com>
2021-05-29 01:15:42 +0300

    A minor fix!

--- src/Pages/Others/TCs.js
+++ src/Pages/Others/TCs.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
-
-const schema = yup.object().shape({
-  name: yup.string().required(),
-  age: yup.number().required(),
-});
 
 const TCs = () => {
-  const { register, handleSubmit } = useForm({
-    resolver: yupResolver(schema),
-  });
-
   return (
-    <form onSubmit={handleSubmit((d) => console.log(d))} className='w-full px-8 grid place-items-center space-y-3'>
-      <input name="name" Ref={register} className='input' placeholder='your name'/>
-      <input name="age" type="number" Ref={register} className='input' placeholder='your age'/>
-      <input type="submit" className='btn-plain px-2' />
-    </form>
+    <div className="w-full p-30 grid place-items-center">
+      <h1> 🥺 Oops, we're still figuring this out...</h1>
+    </div>
   );
 };
 
